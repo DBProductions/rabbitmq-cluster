bcd224da8088ae8cfc5c8c90341e5841cae62ca4
DBProductions <dennis@dbproductions.de>
2019-07-29 23:42:48 +0200

    add federation and shovel config to team example
    adjust README
    
    adjust README
    
    changed federation example
    
    adjust README
    
    removed unused plugins

--- scripts/setup_federation.sh
+++ scripts/setup_federation.sh
@@ -2,21 +2,27 @@
 
 set -e
 
+# user
+docker-compose exec rabbitmq2 rabbitmqctl add_user federal federal
+docker-compose exec rabbitmq2 rabbitmqctl set_permissions -p vhost federal ".*" ".*" ".*"
+
+docker-compose exec rabbitmq3 rabbitmqctl add_user federal federal
+docker-compose exec rabbitmq3 rabbitmqctl set_permissions -p vhost federal ".*" ".*" ".*"
+
 # topology
 docker-compose exec rabbitmq1 rabbitmqadmin -u rabbit -p rabbit -V vhost declare exchange name=rabbitmq2.federated.events type=topic
-docker-compose exec rabbitmq1 rabbitmqadmin -u rabbit -p rabbit -V vhost declare exchange name=rabbitmq3.federated.events type=topic
 docker-compose exec rabbitmq1 rabbitmqadmin -u rabbit -p rabbit -V vhost declare queue name=system.events durable=true
+docker-compose exec rabbitmq1 rabbitmqadmin -u rabbit -p rabbit -V vhost declare queue name=federated.events durable=true
 docker-compose exec rabbitmq1 rabbitmqadmin -u rabbit -p rabbit -V vhost declare binding source="rabbitmq2.federated.events" destination_type="queue" destination="system.events" routing_key="system.create.event"
-docker-compose exec rabbitmq1 rabbitmqadmin -u rabbit -p rabbit -V vhost declare binding source="rabbitmq3.federated.events" destination_type="queue" destination="system.events" routing_key="system.create.event"
 
 # upstream for rabbitmq2
-config='{"max-hops": 1, "uri": "amqp://rabbit:rabbit@rabbitmq2:5672/vhost", "ack-mode":"on-publish"}'
+config='{"uri": "amqp://federal:federal@rabbitmq2:5672/vhost", "ack-mode":"on-confirm", "reconnect-delay": 1, "prefetch-count": 200, "max-hops": 1}'
 docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost federation-upstream rabbitmq2 "${config}"
 config='[{"upstream": "rabbitmq2"}]'
 docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost federation-upstream-set rabbitmq2 "${config}"
 
 # upstream for rabbitmq3
-config='{"max-hops": 1, "uri": "amqp://rabbit:rabbit@rabbitmq3:5672/vhost", "ack-mode":"on-publish"}'
+config='{"uri": "amqp://federal:federal@rabbitmq3:5672/vhost", "ack-mode":"on-confirm", "reconnect-delay": 1, "prefetch-count": 200}'
 docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost federation-upstream rabbitmq3 "${config}"
 config='[{"upstream": "rabbitmq3"}]'
 docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost federation-upstream-set rabbitmq3 "${config}"
@@ -29,6 +35,6 @@ docker-compose exec rabbitmq1 rabbitmqctl set_policy -p vhost ha-federation "^fe
 
 # set policy
 config='{"federation-upstream-set": "rabbitmq3"}'
-docker-compose exec rabbitmq1 rabbitmqctl set_policy -p vhost --apply-to exchanges federation_rabbitmq3 "rabbitmq3.federated.*" "${config}"
+docker-compose exec rabbitmq1 rabbitmqctl set_policy -p vhost --apply-to queues federation_rabbitmq3 "federated.*" "${config}"
 config='{"ha-mode": "all"}'
 docker-compose exec rabbitmq1 rabbitmqctl set_policy -p vhost ha-federation "^federation:*" "${config}"
--- scripts/setup_team.sh
+++ scripts/setup_team.sh
@@ -34,3 +34,31 @@ docker-compose exec rabbitmq2 rabbitmqctl set_permissions -p vhost serviceA "" "
 docker-compose exec rabbitmq2 rabbitmqctl set_permissions -p vhost serviceB "" ".*" ""
 docker-compose exec rabbitmq2 rabbitmqctl set_permissions -p vhost monitor "" "" ""
 
+# federation
+## topology
+docker-compose exec rabbitmq1 rabbitmqadmin -u teamA -p teamA -V vhost declare exchange name=rabbitmq2.federated.events type=topic
+docker-compose exec rabbitmq1 rabbitmqadmin -u teamA -p teamA -V vhost declare queue name=system.events durable=true
+docker-compose exec rabbitmq1 rabbitmqadmin -u teamA -p teamA -V vhost declare binding source="rabbitmq2.federated.events" destination_type="queue" destination="system.events" routing_key="system.create.event"
+
+## upstream
+config='{"max-hops": 1, "uri": "amqp://teamB:teamB@rabbitmq2:5672/vhost", "ack-mode":"on-publish"}'
+docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost federation-upstream rabbitmq2 "${config}"
+config='[{"upstream": "rabbitmq2"}]'
+docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost federation-upstream-set rabbitmq2 "${config}"
+
+## set policy
+config='{"federation-upstream-set": "rabbitmq2"}'
+docker-compose exec rabbitmq1 rabbitmqctl set_policy -p vhost --apply-to exchanges federation_rabbitmq2 "rabbitmq2.federated.*" "${config}"
+config='{"ha-mode": "all"}'
+docker-compose exec rabbitmq1 rabbitmqctl set_policy -p vhost ha-federation "^federation:*" "${config}"
+
+# shovel
+## topology
+docker-compose exec rabbitmq1 rabbitmqadmin -u teamA -p teamA -V vhost declare queue name=shovel durable=true
+docker-compose exec rabbitmq2 rabbitmqadmin -u teamB -p teamB -V vhost declare exchange name=rabbitmq1.shovel type=direct
+docker-compose exec rabbitmq2 rabbitmqadmin -u teamB -p teamB -V vhost declare queue name=shovel durable=true
+docker-compose exec rabbitmq2 rabbitmqadmin -u teamB -p teamB -V vhost declare binding source="rabbitmq1.shovel" destination_type="queue" destination="shovel" routing_key="shovel"
+
+## shovel 1 to 2
+config='{"src-protocol": "amqp091", "src-uri": "amqp://teamA:teamA@rabbitmq1:5672/vhost", "src-queue": "shovel", "dest-protocol": "amqp091", "dest-uri": "amqp://teamB:teamB@rabbitmq2:5672/vhost", "dest-exchange": "rabbitmq1.shovel"}'
+docker-compose exec rabbitmq1 rabbitmqctl set_parameter -p vhost shovel rabbitmq2 "${config}"
