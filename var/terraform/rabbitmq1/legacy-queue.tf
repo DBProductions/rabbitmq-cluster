83b757e6c3b7862f97df4494e5247fc37a9ba86d
DBCodes <dennis@dbproductions.de>
2023-02-13 20:45:47 +0100

    Shovel queue to change the queue type without losing messages

---
+++ var/terraform/rabbitmq1/legacy-queue.tf
@@ -0,0 +1,37 @@
+#
+# Legacy queue shovels messages from one queue to another on the same broker.
+# Queue type change and while consumer and publisher changing the messages are not lost.
+#
+
+resource "rabbitmq_queue" "legacy" {
+    name = "legacy_queue"
+    vhost = rabbitmq_vhost.rmqvhost.name
+    settings {
+        durable = false
+        auto_delete = true
+    }
+}
+
+resource "rabbitmq_queue" "new" {
+    name = "new_queue"
+    vhost = rabbitmq_vhost.rmqvhost.name
+    settings {
+        durable     = true
+        auto_delete = false
+        arguments = {
+            "x-queue-type" : "quorum",
+        }
+    }
+}
+
+resource "rabbitmq_shovel" "queue_shovel" {
+    name = "queueShovel"
+    vhost = rabbitmq_vhost.rmqvhost.name
+    info {
+        source_uri = "amqp://rabbit:rabbit@rabbitmq1:5672/vhost"
+        source_queue = "${rabbitmq_queue.legacy.name}"
+        destination_add_forward_headers = true
+        destination_uri = "amqp://rabbit:rabbit@rabbitmq1:5672/vhost"
+        destination_queue = "${rabbitmq_queue.new.name}"
+    }
+}
